990c257f8924dc78227d34bd99b20ff306cb568d
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2025-02-04 00:52:07 +0300

    Psych 0.7x CallbackHandler.hx

---
+++ CallbackHandler.hx
@@ -0,0 +1,36 @@
+package psychlua;
+
+class CallbackHandler
+{
+	public static inline function callback_handler(l:State, fname:String):Int {
+
+		var cbf = callbacks.get(fname);
+
+		if(cbf == null) {
+			return 0;
+		}
+
+		var nparams:Int = Lua.gettop(l);
+		var args:Array<Dynamic> = [];
+
+		for (i in 0...nparams) {
+			args[i] = Convert.fromLua(l, i + 1);
+		}
+
+		var ret:Dynamic = null;
+
+		if (nparams <= 0) {
+			ret = cbf();
+		} else {
+			ret = Reflect.callMethod(null, cbf, args);
+		}
+
+		if(ret != null){
+			Convert.toLua(l, ret);
+		}
+
+		/* return the number of results */
+		return 1;
+
+	} //callback_handler
+}
