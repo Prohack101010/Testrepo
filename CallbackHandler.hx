8db0003b2a63bf0bfae4d17ebb89002d5a4f2263
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2025-02-04 00:55:23 +0300

    Just CallbackHandler.hx

--- CallbackHandler.hx
+++ CallbackHandler.hx
@@ -2,29 +2,55 @@ package psychlua;
 
 class CallbackHandler
 {
-	public static inline function callback_handler(l:State, fname:String):Int {
+	public static inline function call(l:State, fname:String):Int
+	{
+		try
+		{
+			//trace('calling $fname');
+			var cbf:Dynamic = Lua_helper.callbacks.get(fname);
 
-		var cbf = callbacks.get(fname);
+			//Local functions have the lowest priority
+			//This is to prevent a "for" loop being called in every single operation,
+			//so that it only loops on reserved/special functions
+			if(cbf == null) 
+			{
+				//trace('looping thru scripts');
+				for (script in PlayState.instance.luaArray)
+					if(script != null && script.lua == l)
+					{
+						//trace('found script');
+						cbf = script.callbacks.get(fname);
+						break;
+					}
+			}
+			
+			if(cbf == null) return 0;
 
-		if(cbf == null) {
-			return 0;
+			var nparams:Int = Lua.gettop(l);
+			var args:Array<Dynamic> = [];
+
+			for (i in 0...nparams) {
+				args[i] = Convert.fromLua(l, i + 1);
+			}
+
+            /*
+			var ret:Dynamic = null;
+			/* return the number of results */
+
+			ret = Reflect.callMethod(null,cbf,args);
+			*/
+			var ret:Dynamic = Reflect.callMethod(null, cbf, args);
+
+			if(ret != null){
+				Convert.toLua(l, ret);
+				return 1;
+			}
 		}
-
-		var nparams:Int = Lua.gettop(l);
-		var args:Array<Dynamic> = [];
-
-		for (i in 0...nparams) {
-			args[i] = Convert.fromLua(l, i + 1);
+		catch(e:Dynamic)
+		{
+			trace(e);
+			throw(e);
 		}
-
-		var ret:Dynamic = Reflect.callMethod(null, cbf, args);
-
-		if(ret != null){
-			Convert.toLua(l, ret);
-		}
-
-		/* return the number of results */
-		return 1;
-
-	} //callback_handler
+		return 0;
+	}
 }
