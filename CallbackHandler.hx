f4d69d357597514b4da7b58eba75327d87d28061
Prohack101010 <126958051+Prohack101010@users.noreply.github.com>
2025-02-04 01:10:43 +0300

    Update CallbackHandler.hx

--- CallbackHandler.hx
+++ CallbackHandler.hx
@@ -2,13 +2,27 @@ package psychlua;
 
 class CallbackHandler
 {
-	public static inline function callback_handler(l:State, fname:String):Int {
+	public static inline function call(l:State, fname:String):Int
+	{
+		//trace('calling $fname');
+		var cbf:Dynamic = Lua_helper.callbacks.get(fname);
 
-		var cbf = callbacks.get(fname);
-
-		if(cbf == null) {
-			return 0;
+		//Local functions have the lowest priority
+		//This is to prevent a "for" loop being called in every single operation,
+		//so that it only loops on reserved/special functions
+		if(cbf == null) 
+		{
+			//trace('looping thru scripts');
+			for (script in PlayState.instance.luaArray)
+				if(script != null && script.lua == l)
+				{
+					//trace('found script');
+					cbf = script.callbacks.get(fname);
+					break;
+				}
 		}
+			
+		if(cbf == null) return 0;
 
 		var nparams:Int = Lua.gettop(l);
 		var args:Array<Dynamic> = [];
@@ -17,14 +31,16 @@ class CallbackHandler
 			args[i] = Convert.fromLua(l, i + 1);
 		}
 
+        /* this code doesn't work because I'm using Sirox's linc_luajit
+		var ret:Dynamic = null;
+
+		ret = Reflect.callMethod(null,cbf,args);
+		*/
 		var ret:Dynamic = Reflect.callMethod(null, cbf, args);
 
 		if(ret != null){
 			Convert.toLua(l, ret);
 		}
-
-		/* return the number of results */
 		return 1;
-
-	} //callback_handler
+	}
 }
